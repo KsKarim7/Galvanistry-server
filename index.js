232b35284d115fe97a35d4f8210843c28ea98e13
Khaled Karim <kskarim177@gmail.com>
2022-05-22 18:29:52 +0600

    jwt implemented in entrypoints

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ require('dotenv').config()
 const jwt = require('jsonwebtoken');
 const port = process.env.PORT || 5000
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const { decode } = require('jsonwebtoken');
 
 // middleware
 app.use(cors())
@@ -15,12 +16,32 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@clu
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 // console.log(uri)
 
+// JWT 
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        console.log(authHeader)
+        return res.status(401).send({ message: 'Unauthorized Access' });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden Access' });
+        }
+        req.decoded = decoded;
+        console.log('decod', decoded);
+        next();
+    })
+}
+
 async function run() {
     try {
         await client.connect()
         // console.log("database connected")
 
+        // db collections
         const productsCollection = client.db('galva_nistry').collection('products')
+        const userCollection = client.db('galva_nistry').collection('users')
 
         app.get('/products', async (req, res) => {
             const query = {};
@@ -28,6 +49,20 @@ async function run() {
             const data = await cursor.toArray()
             res.send(data)
         });
+
+        // update user's email
+        app.put('/user/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = req.body;
+            const filter = { email: email };
+            const options = { upsert: true };
+            const updatedDoc = {
+                $set: user,
+            };
+            const result = await userCollection.updateOne(filter, updatedDoc, options);
+            const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1d' })
+            res.send({ result, token })
+        })
 
     }
     finally {
