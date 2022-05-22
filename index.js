92fc25f23c6ddd9b6a272f95a4edd8e3d0790166
Khaled Karim <kskarim177@gmail.com>
2022-05-22 14:30:26 +0600

    product data loaded from database

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const port = process.env.PORT || 5000
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
 // middleware
-app.use(express.json())
 app.use(cors())
+app.use(express.json())
 
 // connect to database
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.gnyaq.mongodb.net/?retryWrites=true&w=majority`;
@@ -18,21 +18,26 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         await client.connect()
-        console.log("database connected")
+        // console.log("database connected")
+
+        const productsCollection = client.db('galva_nistry').collection('products')
+
+        app.get('/products', async (req, res) => {
+            const query = {};
+            const cursor = productsCollection.find(query)
+            const data = await cursor.toArray()
+            res.send(data)
+        });
+
     }
-    finally { }
+    finally {
+
+    }
 }
+
+
+
 run().catch(console.dir)
-
-
-
-
-
-
-
-
-
-
 
 app.get('/', (req, res) => {
     res.send('Hey there')
